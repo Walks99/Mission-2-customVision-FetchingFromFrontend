ea8144b85c9b40cd5f2d1dbd21e351a09f1cda34
Ben Walker <benwalker1.personal@gmail.com>
2023-12-03 12:12:43 +1300

    added a link button on the home page to navigate to the Find simlar cars page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <p>Home Page</p>
+      <Link to="/findsimilarcars">
+        <button>Go to find similar cars page</button>
+      </Link>
+    </div>
   )
 }
 
 export default HomePage
+
+
